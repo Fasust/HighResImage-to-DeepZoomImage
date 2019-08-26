1a6ca01fb74ca4a96b1e01ea7233d2304015ecbf
Sebastian Faust <sebastianf333@googlemail.com>
2019-08-26 14:32:04 +0200

    cutting local images

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 //Modules ---
-const express = require('express');
-const sharp = require('sharp');
+const express = require("express");
+const sharp = require("sharp");
 const app = express();
 
 const PORT = 3000;
+const DEFAULT_TILE_SIZE = 512;
+const DZI_OUTPUT = "img/output";
 
 //Routes ---
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get("/", function(req, res) {
+  sharp("input.jpg")
+    .png()
+    .tile({
+      size: DEFAULT_TILE_SIZE
+    })
+    .toFile(DZI_OUTPUT, function(err, info) {
+      // output.dzi is the Deep Zoom XML definition
+      // output_files contains 512x512 tiles grouped by zoom level
+      console.log(
+        "[LOG] Image successfully converted | schema: " +
+          DZI_OUTPUT +
+          ".dzi | files: " +
+          DZI_OUTPUT +
+          "_files"
+      );
+      res.send("Image has ben successfully converted");
+    });
 });
 
 //Open Service ---
-app.listen(PORT, function () {
-  console.log('Opened Service on Port: ' + PORT);
-}); 
+app.listen(PORT, function() {
+  console.log("[LOG] Opened Service on Port: " + PORT);
+});
