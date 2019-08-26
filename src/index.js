0c8d924c3035040d3f311cc22c7aefd5d8f11985
Sebastian Faust <sebastianf333@googlemail.com>
2019-08-26 15:56:59 +0200

    receiving image & processing them & saving them locally

--- src/index.js
+++ src/index.js
@@ -1,31 +1,63 @@
 //Modules ---
 const express = require("express");
 const sharp = require("sharp");
+const fileUpload = require("express-fileupload");
 const app = express();
+
+// default options
+app.use(fileUpload());
 
 const PORT = 3000;
 const DEFAULT_TILE_SIZE = 512;
-const DZI_OUTPUT = "img/output.zip";
+const OUTPUT_DIR = "output/";
+const OUTPUT_FILE_NAME = "output";
+const OUTPUT_FILE_EXTENSION = ".zip";
+const INPUT_DIR = "input/";
 
 //Routes ---
-app.get("/", function(req, res) {
-  sharp("test-input.jpg")
-    .png()
-    .tile({
-      size: DEFAULT_TILE_SIZE
-    })
-    .toFile(DZI_OUTPUT, function(err, info) {
-      // output.dzi is the Deep Zoom XML definition
-      // output_files contains 512x512 tiles grouped by zoom level
-      console.log(
-        "[LOG] Image successfully converted | schema: " +
-          DZI_OUTPUT +
-          "/output/output.dzi | files: " +
-          DZI_OUTPUT +
-          "/output/output_files"
-      );
-      res.send("Image has ben successfully converted");
-    });
+app.post("/", function(req, res) {
+  if (Object.keys(req.files).length == 0) {
+    return res.status(400).send("No files were uploaded.");
+  }
+
+  let img = req.files.file;
+
+  // Use the mv() method to place the file somewhere on your server
+  img.mv(INPUT_DIR + "in.jpg", function(err) {
+    if (err) return res.status(500).send(err);
+
+    console.log("[LOG] Upload successful");
+
+    sharp(INPUT_DIR + "in.jpg")
+      .png()
+      .tile({
+        size: DEFAULT_TILE_SIZE
+      })
+      .toFile(OUTPUT_DIR + OUTPUT_FILE_NAME + OUTPUT_FILE_EXTENSION, function(err, info) {
+        // output.dzi is the Deep Zoom XML definition
+        // output_files contains 512x512 tiles grouped by zoom level
+        console.log(
+          "[LOG] Image successfully converted | schema: " +
+            OUTPUT_DIR +
+            OUTPUT_FILE_NAME +
+            OUTPUT_FILE_EXTENSION +
+            "/" +
+            OUTPUT_FILE_NAME +
+            "/" +
+            OUTPUT_FILE_NAME +
+            ".dzi | files: " +
+            OUTPUT_DIR +
+            OUTPUT_FILE_NAME +
+            OUTPUT_FILE_EXTENSION +
+            "/" +
+            OUTPUT_FILE_NAME +
+            "/" +
+            OUTPUT_FILE_NAME +
+            "_files"
+        );
+        res.send("Image has ben successfully converted");
+      });
+  });
 });
 
 //Open Service ---
